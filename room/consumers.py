974a0b9139f6607e554ea282e5f96479fb47414c
ChocTitans <sampcfrpg@live.fr>
2023-01-22 00:35:03 +0100

    Update consumers.py

--- room/consumers.py
+++ room/consumers.py
@@ -4,9 +4,11 @@ from django.contrib.auth.models import User
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 import emoji
+from django.views.decorators.csrf import csrf_protect
 
 from .models import Room, Message
 
+@csrf_protect
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
